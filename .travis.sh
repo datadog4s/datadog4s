5644592916eb1905cd13afe5d1490ca2a640ed0c
Jan Strnad <strnad@avast.com>
2019-08-18 19:50:35 +0200

    travis

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -x
+
 sbt ++$TRAVIS_SCALA_VERSION clean scalafmtCheck test docs/mdoc &&
  if $(test ${TRAVIS_REPO_SLUG} == "avast/datadog-scala-metrics" && test ${TRAVIS_PULL_REQUEST} == "false" && test "$TRAVIS_TAG" != ""); then
    sbt +publish
