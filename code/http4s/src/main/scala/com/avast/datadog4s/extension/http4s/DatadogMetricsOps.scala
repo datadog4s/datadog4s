c1900846ffdc7e9419779dde86230f531d2d5838
Tomas Herman <hermant@avast.com>
2019-08-30 22:21:22 +0200

    Http4s datadog

--- code/http4s/src/main/scala/com/avast/datadog4s/extension/http4s/DatadogMetricsOps.scala
+++ code/http4s/src/main/scala/com/avast/datadog4s/extension/http4s/DatadogMetricsOps.scala
@@ -4,6 +4,7 @@ import java.time.Duration
 
 import cats.syntax.flatMap._
 import cats.effect.Sync
+import com.avast.datadog4s.api.tag.Tagger
 import com.avast.datadog4s.api.{ MetricFactory, Tag }
 import org.http4s.{ Method, Status }
 import org.http4s.metrics.{ MetricsOps, TerminationType }
@@ -18,7 +19,10 @@ object DatadogMetricsOps {
     implicit F: Sync[F]
   ): MetricsOps[F] = new MetricsOps[F] {
 
-    private[this] val activeRequests = metricFactory.count("active_requests")
+    private[this] val methodTag       = Tagger.make[String]("method")
+    private[this] val statusBucketTag = Tagger.make[String]("status_bucket")
+    private[this] val typeTag         = Tagger.make[String]("type")
+    private[this] val activeRequests  = metricFactory.count("active_requests")
 
     override def increaseActiveRequests(classifier: Option[String]): F[Unit] =
       activeRequests.inc(classifier.toList.flatMap(classifierTags): _*)
@@ -32,14 +36,15 @@ object DatadogMetricsOps {
       headersTime
         .record(
           Duration.ofNanos(elapsed),
-          Tag.of("method", method.name) :: classifier.toList.flatMap(classifierTags): _*
+          methodTag.tag(method.name) :: classifier.toList.flatMap(classifierTags): _*
         )
 
     private[this] val requestCount   = metricFactory.count("requests_count")
     private[this] val requestLatency = metricFactory.timer("requests_latency")
     override def recordTotalTime(method: Method, status: Status, elapsed: Long, classifier: Option[String]): F[Unit] = {
-      val tags = Tag.of("method", method.name) :: Tag
-        .of("response_code", status.code.toString) :: classifier.toList.flatMap(classifierTags)
+      val tags = methodTag.tag(method.name) ::
+        statusToBucketTag(status) ::
+        Tag.of("response_code", status.code.toString) :: classifier.toList.flatMap(classifierTags)
       requestCount.inc(tags: _*) >> requestLatency.record(Duration.ofNanos(elapsed), tags: _*)
     }
 
@@ -51,13 +56,22 @@ object DatadogMetricsOps {
       classifier: Option[String]
     ): F[Unit] = {
       val tpe = terminationType match {
-        case TerminationType.Abnormal => "abnormal"
-        case TerminationType.Error    => "error"
-        case TerminationType.Timeout  => "timeout"
+        case TerminationType.Abnormal => typeTag.tag("abnormal")
+        case TerminationType.Error    => typeTag.tag("error")
+        case TerminationType.Timeout  => typeTag.tag("timeout")
       }
-      val tags = Tag.of("type", tpe) :: classifier.toList.flatMap(classifierTags)
+      val tags = tpe :: classifier.toList.flatMap(classifierTags)
       abnormalCount.inc(tags: _*) >> abnormalLatency.record(Duration.ofNanos(elapsed), tags: _*)
     }
+
+    private def statusToBucketTag(status: Status): Tag =
+      status.code match {
+        case x if x < 200 => statusBucketTag.tag("1xx")
+        case x if x < 300 => statusBucketTag.tag("2xx")
+        case x if x < 400 => statusBucketTag.tag("3xx")
+        case x if x < 500 => statusBucketTag.tag("4xx")
+        case x if x < 600 => statusBucketTag.tag("5xx")
+      }
   }
 
 }
