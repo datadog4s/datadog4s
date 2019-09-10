90f8ad6de0d23851a89edd6b69be534b4ff72b1a
Scala Steward <me@scala-steward.org>
2019-09-10 06:26:36 +0200

    Update cats-core to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Cats {
-    val core   = "org.typelevel" %% "cats-core"   % "1.6.1"
+    val core   = "org.typelevel" %% "cats-core"   % "2.0.0"
     val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
   }
 
