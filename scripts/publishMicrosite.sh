8a5e4648adf273f3661c6a6d9a5498e0990e8d09
Tomas Herman <hermant@avast.com>
2020-01-17 13:29:27 +0100

    Updated microsite deploys

---
+++ scripts/publishMicrosite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+git config --global user.email "travis-job@travis-ci.com"
+git config --global user.name "Travis CI"
+git config --global push.default simple
+
+sbt docs/publishMicrosite
