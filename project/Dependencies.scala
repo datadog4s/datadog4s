ce14a4dd6eb83f4ee9c641e8e18d2b9ecdda8235
Scala Steward <me@scala-steward.org>
2020-02-21 16:10:10 +0100

    Update mockito-scala-scalatest to 1.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
 
   object Testing {
     val scalaTest        = "org.scalatest" %% "scalatest"               % "3.1.1"
-    val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % "1.11.2"
+    val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % "1.11.3"
   }
 
   object Mdoc {
