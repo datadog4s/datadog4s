aedc3121e16cfa571a0f56f96902f4642b2c68e4
Scala Steward <me@scala-steward.org>
2020-02-21 16:10:03 +0100

    Update silencer-lib, silencer-plugin to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   }
 
   object Silencer {
-    val lib    = "com.github.ghik" % "silencer-lib" % "1.5.0" % Provided cross CrossVersion.full
-    val plugin = compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.5.0" cross CrossVersion.full)
+    val lib    = "com.github.ghik" % "silencer-lib" % "1.6.0" % Provided cross CrossVersion.full
+    val plugin = compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.6.0" cross CrossVersion.full)
   }
 }
