ad03e209f01baf7dd229a8e7762ae2128b834be3
Tomáš Heřman <hermant@avast.com>
2020-03-22 11:59:56 +0100

    Added docs

--- code/common/src/main/scala/com/avast/cloud/datadog4s/helpers/Repeated.scala
+++ code/common/src/main/scala/com/avast/cloud/datadog4s/helpers/Repeated.scala
@@ -11,6 +11,14 @@ import cats.syntax.applicative._
 import scala.concurrent.duration._
 
 object Repeated {
+
+  /**
+   * Creates a never ending process that will run [[task]] periodically. The resource provided has the same semantics as [[Concurrent.background()]]
+   * @param delay Delay between each run of [[task]]
+   * @param iterationTimeout timeout applied to [[task]], ideally it should be lower than [[delay]]
+   * @param errorHandler handler called when [[task]] fails or during timeout
+   * @param task effect that will be run periodically
+   */
   def run[F[_]: Concurrent: Timer](
     delay: Duration,
     iterationTimeout: Duration,
