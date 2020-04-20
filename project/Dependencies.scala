2ba75c6d23f8dda467cda7195022f0d6fdba263d
Scala Steward <me@scala-steward.org>
2020-04-20 18:12:12 +0200

    Update scala-collection-compat to 2.1.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   }
 
   object ScalaModules {
-    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.5"
+    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.6"
   }
 
   object Silencer {
