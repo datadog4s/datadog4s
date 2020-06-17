69b1a943eeb5f84502312b718008a915726b5976
Jan Strnad <strnad@avast.com>
2020-06-17 12:46:38 +0200

    feat(jvm): made metric names compatible with JMX collector

--- code/jvm/src/main/scala/com/avast/datadog4s/extension/jvm/JvmReporter.scala
+++ code/jvm/src/main/scala/com/avast/datadog4s/extension/jvm/JvmReporter.scala
@@ -19,20 +19,25 @@ class JvmReporter[F[_]: Sync](metricsFactory: MetricFactory[F]) {
   private val cpuLoad              = metricsFactory.gauge.double("jvm.cpu.load")
   private val cpuTime              = metricsFactory.gauge.long("jvm.cpu.time")
   private val openFds              = metricsFactory.gauge.long("jvm.filedescriptor.open")
-  private val heapUsed             = metricsFactory.gauge.long("jvm.heap.used")
-  private val heapCommitted        = metricsFactory.gauge.long("jvm.heap.committed")
-  private val heapMax              = metricsFactory.gauge.long("jvm.heap.max")
-  private val nonHeapUsed          = metricsFactory.gauge.long("jvm.nonheap.used")
-  private val nonHeapCommitted     = metricsFactory.gauge.long("jvm.nonheap.committed")
+  private val heapUsed             = metricsFactory.gauge.long("jvm.heap_memory")
+  private val heapCommitted        = metricsFactory.gauge.long("jvm.heap_memory_committed")
+  private val heapInit             = metricsFactory.gauge.long("jvm.heap_memory_init")
+  private val heapMax              = metricsFactory.gauge.long("jvm.heap_memory_max")
+  private val nonHeapUsed          = metricsFactory.gauge.long("jvm.non_heap_memory")
+  private val nonHeapCommitted     = metricsFactory.gauge.long("jvm.non_heap_memory_committed")
+  private val nonHeapInit          = metricsFactory.gauge.long("jvm.non_heap_memory_init")
+  private val nonHeapMax           = metricsFactory.gauge.long("jvm.non_heap_memory_max")
   private val uptime               = metricsFactory.gauge.long("jvm.uptime")
-  private val threadsTotal         = metricsFactory.gauge.long("jvm.threads.total")
-  private val threadsDaemon        = metricsFactory.gauge.long("jvm.threads.daemon")
-  private val threadsStarted       = metricsFactory.gauge.long("jvm.threads.started")
+  private val threadsTotal         = metricsFactory.gauge.long("jvm.thread_count")
+  private val threadsDaemon        = metricsFactory.gauge.long("jvm.thread_daemon")
+  private val threadsStarted       = metricsFactory.gauge.long("jvm.thread_started")
   private val classes              = metricsFactory.gauge.long("jvm.classes.loaded")
   private val bufferPoolsInstances = metricsFactory.gauge.long("jvm.bufferpool.instances")
   private val bufferPoolsBytes     = metricsFactory.gauge.long("jvm.bufferpool.bytes")
-  private val gcCollections        = metricsFactory.gauge.long("jvm.gc.collections")
-  private val gcTime               = metricsFactory.gauge.long("jvm.gc.time")
+  private val gcMinorCollections   = metricsFactory.gauge.long("jvm.gc.minor_collection_count")
+  private val gcMinorTime          = metricsFactory.gauge.long("jvm.gc.minor_collection_time")
+  private val gcMajorCollections   = metricsFactory.gauge.long("jvm.gc.major_collection_count")
+  private val gcMajorTime          = metricsFactory.gauge.long("jvm.gc.major_collection_time")
 
   private val osBean      = F.delay(ManagementFactory.getOperatingSystemMXBean.asInstanceOf[OperatingSystemMXBean])
   private val unixBean    = F.delay(ManagementFactory.getOperatingSystemMXBean.asInstanceOf[UnixOperatingSystemMXBean])
@@ -48,9 +53,14 @@ class JvmReporter[F[_]: Sync](metricsFactory: MetricFactory[F]) {
 
   private val gc: Vector[F[Unit]] =
     gcBeans.map { bean =>
+      val name   = bean.getName
       val gcName = Tag.of("gc_name", bean.getName.replace(" ", "_"))
-      wrapUnsafe(gcCollections, gcName)(bean.getCollectionCount) >>
-        wrapUnsafe(gcTime, gcName)(bean.getCollectionTime)
+      if (name.contains("young"))
+        wrapUnsafe(gcMinorCollections, gcName)(bean.getCollectionCount) >>
+          wrapUnsafe(gcMinorTime, gcName)(bean.getCollectionTime)
+      else
+        wrapUnsafe(gcMajorCollections, gcName)(bean.getCollectionCount) >>
+          wrapUnsafe(gcMajorTime, gcName)(bean.getCollectionTime)
     }
 
   private val buffers: Vector[F[Unit]] = bufferBeans.map { bean =>
@@ -66,9 +76,12 @@ class JvmReporter[F[_]: Sync](metricsFactory: MetricFactory[F]) {
   protected[jvm] val getOpenFDsCountIO     = protect(unixBean)(bean => wrapUnsafe(openFds)(bean.getOpenFileDescriptorCount))
   protected[jvm] val getHeapUsedIO         = wrapUnsafe(heapUsed)(memoryBean.getHeapMemoryUsage.getUsed)
   protected[jvm] val getHeapCommittedIO    = wrapUnsafe(heapCommitted)(memoryBean.getHeapMemoryUsage.getCommitted)
+  protected[jvm] val getHeapInitIO         = wrapUnsafe(heapInit)(memoryBean.getHeapMemoryUsage.getInit)
   protected[jvm] val getHeapMaxIO          = wrapUnsafe(heapMax)(memoryBean.getHeapMemoryUsage.getMax)
   protected[jvm] val getNonHeapCommittedIO = wrapUnsafe(nonHeapCommitted)(memoryBean.getNonHeapMemoryUsage.getCommitted)
   protected[jvm] val getNonHeapUsedIO      = wrapUnsafe(nonHeapUsed)(memoryBean.getNonHeapMemoryUsage.getUsed)
+  protected[jvm] val getNonHeapInitIO      = wrapUnsafe(nonHeapInit)(memoryBean.getNonHeapMemoryUsage.getInit)
+  protected[jvm] val getNonHeapMaxIO       = wrapUnsafe(nonHeapMax)(memoryBean.getNonHeapMemoryUsage.getMax)
   protected[jvm] val getUptimeIO           = wrapUnsafe(uptime)(runtimeBean.getUptime)
   protected[jvm] val getThreadsTotalIO     = wrapUnsafe(threadsTotal)(threadBean.getThreadCount.toLong)
   protected[jvm] val getThreadsDaemonIO    = wrapUnsafe(threadsDaemon)(threadBean.getDaemonThreadCount.toLong)
@@ -88,9 +101,12 @@ class JvmReporter[F[_]: Sync](metricsFactory: MetricFactory[F]) {
       getOpenFDsCountIO >>
       getHeapUsedIO >>
       getHeapCommittedIO >>
+      getHeapInitIO >>
       getHeapMaxIO >>
       getNonHeapCommittedIO >>
       getNonHeapUsedIO >>
+      getNonHeapInitIO >>
+      getNonHeapInitIO >>
       getUptimeIO >>
       getThreadsTotalIO >>
       getThreadsDaemonIO >>
