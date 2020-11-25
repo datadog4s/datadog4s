c848397d8bcade1b20f844a294313dd027d25bba
Scala Steward <me@scala-steward.org>
2020-11-25 06:09:59 +0100

    Update scala-collection-compat to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   }
 
   object ScalaModules {
-    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0"
+    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.3.1"
   }
 
 }
