3289e44c18a837cb66ceae5d967ed7cef029a98f
Scala Steward <me@scala-steward.org>
2021-01-19 20:09:39 +0100

    Update mdoc, sbt-mdoc to 2.2.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   }
 
   object Mdoc {
-    val libMdoc = "org.scalameta" %% "mdoc" % "2.2.14" excludeAll (ExclusionRule(
+    val libMdoc = "org.scalameta" %% "mdoc" % "2.2.16" excludeAll (ExclusionRule(
       organization = "org.slf4j"
     ))
   }
