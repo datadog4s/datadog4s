9a06b173266c781d1feb5bff1a611e760013ec7e
Tomáš Heřman <tomas.herman@avast.com>
2021-03-26 23:57:19 +0100

    Scala 3.0.0-RC1 release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   object Testing {
     val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % "1.15.1"
-    val munit            = "org.scalameta" %% "munit"                   % "0.7.21"
+    val munit            = "org.scalameta" %% "munit"                   % "0.7.22"
   }
 
   object Logging {
@@ -29,7 +29,7 @@ object Dependencies {
   }
 
   object ScalaModules {
-    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.3.1"
+    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.2"
   }
 
 }
