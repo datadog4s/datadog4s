22c269589409197737d7454a77f1b4ec72a3fd3f
Scala Steward <me@scala-steward.org>
2021-04-20 20:13:20 +0200

    Update cats-effect to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 object Dependencies {
   object Cats {
     val core   = "org.typelevel" %% "cats-core"   % "2.6.0"
-    val effect = "org.typelevel" %% "cats-effect" % "2.5.0"
+    val effect = "org.typelevel" %% "cats-effect" % "3.1.0"
   }
 
   object Datadog {
