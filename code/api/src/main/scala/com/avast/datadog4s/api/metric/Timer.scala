d6824ac19a4577e4747285109ba7fee357114bbc
Tomáš Heřman <tomas.herman@avast.com>
2021-04-22 19:49:09 +0200

    Added docs

--- code/api/src/main/scala/com/avast/datadog4s/api/metric/Timer.scala
+++ code/api/src/main/scala/com/avast/datadog4s/api/metric/Timer.scala
@@ -8,15 +8,22 @@ import java.util.concurrent.TimeUnit
 trait Timer[F[_]] {
   def time[A](f: F[A], tags: Tag*): F[A]
 
-  def record(duration: Duration, tags: Tag*): F[Unit]                   = recordT[Duration](duration, tags: _*)
-  def recordMs(duration: Long, timeUnit: TimeUnit, tags: Tag*): F[Unit] =
+  def record(duration: Duration, tags: Tag*): F[Unit] = recordT[Duration](duration, tags: _*)
+
+  /**
+   * Record raw measurement represented as long
+   * @param duration Measured duration
+   * @param timeUnit TimeUnit in which [[duration]] is measured
+   * @param tags Tags that should be applied for this recording
+   */
+  def recordRaw(duration: Long, timeUnit: TimeUnit, tags: Tag*): F[Unit] =
     recordT[Duration](Duration.ofNanos(timeUnit.toNanos(duration)), tags: _*)
 
   /**
    * Record a value that implements [[com.avast.datadog4s.api.metric.AsDuration]] type class. By default we provide instance for [[java.time.Duration]] and [[scala.concurrent.duration.FiniteDuration]].
    *
    * @param value Representation of duration that should be recorded
-   * @param tags
+   * @param tags Tags that should be applied for this recording
    */
   def recordT[T: AsDuration](value: T, tags: Tag*): F[Unit]
 }
