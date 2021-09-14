cdb366af2409a1953d30f37e73fb7c880b972684
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-15 00:17:28 +0200

    Update scala-library to 2.12.15 (#442)

--- project/BuildSupport.scala
+++ project/BuildSupport.scala
@@ -9,7 +9,7 @@ import sbtdynver.DynVerPlugin.autoImport._
 
 object BuildSupport {
   object ScalaVersions {
-    lazy val scala212               = "2.12.14"
+    lazy val scala212               = "2.12.15"
     lazy val scala213               = "2.13.6"
     lazy val scala3                 = "3.0.1"
     lazy val supportedScalaVersions = List(scala212, scala213, scala3)
