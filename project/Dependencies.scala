1334593f8557ca49b459d9e4e60af0af0f4a1d25
Scala Steward <me@scala-steward.org>
2022-01-30 16:10:07 +0100

    Update mdoc, sbt-mdoc to 2.2.24 in series/cats-effect2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   }
 
   object Mdoc {
-    val libMdoc = "org.scalameta" %% "mdoc" % "2.2.21" excludeAll (ExclusionRule(
+    val libMdoc = "org.scalameta" %% "mdoc" % "2.2.24" excludeAll (ExclusionRule(
       organization = "org.slf4j"
     ))
   }
