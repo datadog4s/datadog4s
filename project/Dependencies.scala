8bce2966b8c0d151d6ea2521cbf2a804e9ea4de2
Scala Steward <me@scala-steward.org>
2022-03-19 00:06:35 +0100

    Update http4s-core to 0.23.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   }
 
   object Http4s {
-    val core = "org.http4s" %% "http4s-core" % "0.23.10"
+    val core = "org.http4s" %% "http4s-core" % "0.23.11"
   }
 
   object Testing {
