ba5935e3fbbfe148061c6bf711239c4cc8cbe8e7
Scala Steward <scala_steward@virtuslab.com>
2022-07-09 15:47:35 +0000

    Update scala3-library to 3.1.3

--- project/BuildSupport.scala
+++ project/BuildSupport.scala
@@ -11,7 +11,7 @@ object BuildSupport {
   object ScalaVersions {
     lazy val scala212               = "2.12.15"
     lazy val scala213               = "2.13.8"
-    lazy val scala3                 = "3.1.2"
+    lazy val scala3                 = "3.1.3"
     lazy val supportedScalaVersions = List(scala212, scala213, scala3)
   }
 
