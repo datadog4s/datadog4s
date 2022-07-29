28b6412b090d6e8913b1c9b677bcecbd2379000f
Scala Steward <scala_steward@virtuslab.com>
2022-07-29 12:20:28 +0000

    Update scala-collection-compat to 2.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   }
 
   object ScalaModules {
-    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.8.0"
+    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % "2.8.1"
   }
 
 }
