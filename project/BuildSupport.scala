ed063cae386abebce0eabd9c90aed3a006e77870
Scala Steward <scala_steward@virtuslab.com>
2024-02-29 14:33:34 +0000

    Update scala-library to 2.12.19

--- project/BuildSupport.scala
+++ project/BuildSupport.scala
@@ -9,7 +9,7 @@ import sbtdynver.DynVerPlugin.autoImport._
 
 object BuildSupport {
   object ScalaVersions {
-    lazy val scala212               = "2.12.18"
+    lazy val scala212               = "2.12.19"
     lazy val scala213               = "2.13.13"
     lazy val scala3                 = "3.3.1"
     lazy val supportedScalaVersions = List(scala212, scala213, scala3)
