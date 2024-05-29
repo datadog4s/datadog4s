12ccd91407d94da73d92dd8c883f022055c545ca
Scala Steward <scala_steward@virtuslab.com>
2024-05-29 13:17:07 +0000

    Update cats-core to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 object Dependencies {
   object Cats {
     val effect = "org.typelevel" %% "cats-effect" % "3.5.4"
-    val core   = "org.typelevel" %% "cats-core"   % "2.10.0"
+    val core   = "org.typelevel" %% "cats-core"   % "2.12.0"
   }
 
   object Datadog {
