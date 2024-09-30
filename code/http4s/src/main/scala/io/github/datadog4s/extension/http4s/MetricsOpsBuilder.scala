b102e0e3df08c29ec663deb73785b713a38ada61
Tomas Herman (toherman) <toherman@cisco.com>
2024-09-30 19:58:39 +0200

    Fix import

--- code/http4s/src/main/scala/io/github/datadog4s/extension/http4s/MetricsOpsBuilder.scala
+++ code/http4s/src/main/scala/io/github/datadog4s/extension/http4s/MetricsOpsBuilder.scala
@@ -5,7 +5,6 @@ import cats.syntax.functor.*
 import io.github.datadog4s.api.{MetricFactory, Tag}
 import DatadogMetricsOps.ClassifierTags
 import impl.{ActiveConnections, DefaultMetricsOps}
-import io.github.datadog4s.api
 import org.http4s.metrics.MetricsOps
 
 import scala.annotation.nowarn
@@ -17,26 +16,26 @@ import scala.annotation.nowarn
     classifierTags: ClassifierTags
 ) {
 
-  /** Force MetricOps to use [[api.TimerFactory.distribution TimerFactory.distribution]] for timing http4s requests. For
-    * the implications please see [[api.TimerFactory.distribution TimerFactory.distribution]] scaladoc.
+  /** Force MetricOps to use [[io.github.datadog4s.api.TimerFactory.distribution TimerFactory.distribution]] for timing http4s requests. For
+    * the implications please see [[io.github.datadog4s.api.TimerFactory.distribution TimerFactory.distribution]] scaladoc.
     */
   def useDistributionBasedTimers(): MetricsOpsBuilder[F] =
     copy(distributionBasedTimers = true)
 
-  /** Force MetricOps to use [[api.TimerFactory.histogram TimerFactory.histogram]] for timing http4s requests. For the
-    * implications please see [[api.TimerFactory.histogram TimerFactory.histogram]] scaladoc.
+  /** Force MetricOps to use [[io.github.datadog4s.api.TimerFactory.histogram TimerFactory.histogram]] for timing http4s requests. For the
+    * implications please see [[io.github.datadog4s.api.TimerFactory.histogram TimerFactory.histogram]] scaladoc.
     */
   def useHistogramBasedTimers(): MetricsOpsBuilder[F] =
     copy(distributionBasedTimers = false)
 
-  /** Force MetricOps to use [[api.DistributionFactory DistributionFactory]] for counting http4s requests. This is
+  /** Force MetricOps to use [[io.github.datadog4s.api.DistributionFactory DistributionFactory]] for counting http4s requests. This is
     * useful in serverless models like AWS Lambda to aggregate all counter values. For more info see
     * [[https://docs.datadoghq.com/serverless/custom_metrics/#understanding-distribution-metrics Datadog documentation]].
     */
   def useDistributionBasedCounters(): MetricsOpsBuilder[F] =
     copy(distributionBasedCounters = true)
 
-  /** Force MetricOps to use [[api.metric.Count Count]] for counting http4s requests. This is sufficient for requests on
+  /** Force MetricOps to use [[io.github.datadog4s.api.metric.Count Count]] for counting http4s requests. This is sufficient for requests on
     * servers where the host is automatically added as a tag. For more info see
     * [[https://docs.datadoghq.com/serverless/custom_metrics/#understanding-distribution-metrics Datadog documentation]].
     */
