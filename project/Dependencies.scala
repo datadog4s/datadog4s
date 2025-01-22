d18f75455de4a5a62f30f4377dd2abefd86ac97b
Scala Steward <scala_steward@virtuslab.com>
2025-01-22 15:37:45 +0000

    Update munit to 1.1.0 in series/cats-effect2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   object Testing {
     val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % "1.15.1"
-    val munit            = "org.scalameta" %% "munit"                   % "1.0.4"
+    val munit            = "org.scalameta" %% "munit"                   % "1.1.0"
   }
 
   object Logging {
