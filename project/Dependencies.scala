b6d2342a4c7afc24e9f10976cf778f9244ec0a77
Scala Steward <scala_steward@virtuslab.com>
2025-04-08 16:43:21 +0000

    Update cats-effect to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 object Dependencies {
   object Cats {
-    val effect = "org.typelevel" %% "cats-effect" % "3.5.7"
+    val effect = "org.typelevel" %% "cats-effect" % "3.6.1"
     val core   = "org.typelevel" %% "cats-core"   % "2.12.0"
   }
 
