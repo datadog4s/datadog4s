e5f8bb2dd9b34116e5fb137fda9a3fd50540521f
Tomas Herman (toherman) <toherman@cisco.com>
2025-09-22 23:12:51 +0200

    Fix compilation

--- code/jvm/src/main/scala/com/avast/datadog4s/extension/jvm/JvmMonitoring.scala
+++ code/jvm/src/main/scala/com/avast/datadog4s/extension/jvm/JvmMonitoring.scala
@@ -1,10 +1,12 @@
 package com.avast.datadog4s.extension.jvm
 
-import java.time.Duration
-
-import cats.effect.{ConcurrentEffect, Resource, Sync, Timer}
+import cats.effect.kernel.Temporal
+import cats.effect.{Resource, Sync}
 import com.avast.cloud.datadog4s.helpers.Repeated
 import com.avast.datadog4s.api.MetricFactory
+import org.typelevel.scalaccompat.annotation.nowarn3
+
+import java.time.Duration
 
 object JvmMonitoring {
   type ErrorHandler[F[_]] = Throwable => F[Unit]
@@ -14,10 +16,12 @@ object JvmMonitoring {
       timeout: Duration = Duration.ofSeconds(10)
   )
 
-  def default[F[_]: ConcurrentEffect: Timer](factory: MetricFactory[F]): Resource[F, Unit] =
+  @nowarn3 // Context bounds in curlies are incompatible with scala 2.12
+  def default[F[_]: Sync: Temporal](factory: MetricFactory[F]): Resource[F, Unit] =
     configured(factory, Config(), defaultErrorHandler)
 
-  def configured[F[_]: ConcurrentEffect: Timer](
+  @nowarn3 // Context bounds in curlies are incompatible with scala 2.12
+  def configured[F[_]: Sync: Temporal](
       factory: MetricFactory[F],
       config: Config,
       errorHandler: ErrorHandler[F]
