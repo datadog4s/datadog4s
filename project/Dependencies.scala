8522784946f877245a2dff9420b7c69e79385c18
Scala Steward <scala_steward@virtuslab.com>
2026-04-13 16:14:06 +0000

    Update munit to 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   object Testing {
     val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % "1.15.1"
-    val munit            = "org.scalameta" %% "munit"                   % "1.2.4"
+    val munit            = "org.scalameta" %% "munit"                   % "1.3.0"
   }
 
   object Logging {
